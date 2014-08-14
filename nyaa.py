392f958ec6375ba498ffd03e99f39c05a63771d8
Alexander Weidinger <alexwegoo@gmail.com>
2014-08-14 14:20:31 +0200

    Added colors like on the original website

--- nyaa.py
+++ nyaa.py
@@ -1,44 +1,66 @@
 #!/usr/bin/env python
-
 import feedparser
 import re
-import os, subprocess
-import sys
 from termcolor import cprint
+import signal
+from subprocess import call
+from sys import exit
+from os import devnull
+import constants
 
-query = input("Which Anime are you looking for?\n")
+def signal_handler(signal, frame):
+	exit(1)
+signal.signal(signal.SIGINT, signal_handler)
 
-search = feedparser.parse('http://www.nyaa.se/?page=search&cats=1_37&filter=0&term=' + query + '&page=rss&sort=2')
+# 1. #Seeders - 2. #Leechers - 3. #Downloads - 4. Size - 5. Additional
+rss_regex = re.compile("(\d+) seeder\(s\), (\d+) leecher\(s\), (\d+) download\(s\) - (\d+(?:\.\d+)? (?:KiB|MiB|GiB))(.*)")
 
-# Group #0 - Number of Seeders | Group #1 - Number of Leechers | Group #2 - Number of Downloads | Group #3 - Size in KiB|MiB|GiB
-regex = re.compile("(\d*) seeder\(s\), (\d*) leecher\(s\), (\d*) download\(s\) - (\d*(?:\.\d*)? (?:KiB|MiB|GiB))(.*)")
+search_query = input("Which Anime are you looking for?\n>> ")
+search_url = constants.URL + "&cats=" + constants.CATS_ANIME_ENGLISH_TRANSLATED + "&filter=" + constants.FILTER_NONE + "&order=" + constants.ORDER_DESCENDING + "&sort=" + constants.SORT_SEEDERS + "&term=" + search_query
+search_result = feedparser.parse(search_url)
 
-print(len(search.entries))
-
-for i in range(0, len(search.entries)):
-	file = search.entries[i]
-	elems = regex.match(file.summary)
-	print(	"[" + str(i) + "]\t" + file.title, end="\n\t")
-	cprint(	"SE " + elems.group(1), 'green', attrs=['bold'], end="\t"),
-	cprint(	"LE " + elems.group(2), 'red', attrs=['bold'], end="\t")
-	print(	"Size " + elems.group(4))
-
-digits = input("select files:\n").split()
-
-NULLF = open(os.devnull, 'w')
-def download(n):
-	print("Adding torrent...", subprocess.call(["transmission-remote-cli", search.entries[n].link], stdout=NULLF))
-
-running = subprocess.call(["pidof", "transmission-daemon"])
-print("'transmission-daemon' is running?...", running)
-if running != 0:
-	print("Trying to start 'transmission-daemon'...", subprocess.call("transmission-daemon"))
-
-for number in digits:
-	if '-' in number:
-		mrange = number.split('-')
-		print("adding files ", mrange[0], "to", mrange[1])
-		for i in range(int(mrange[0]), int(mrange[1]) + 1):
-			download(i)
+# Printing search results
+for i in range(0, len(search_result.entries)):
+	regex_match = rss_regex.match(search_result.entries[i].summary)
+	cprint("[" + str(i) + "]", end='\t')
+	if "Trusted" in regex_match.group(5):
+		if "A+" in regex_match.group(5):
+			cprint(search_result.entries[i].title, 'blue')
+		else:
+			cprint(search_result.entries[i].title, 'green')
 	else:
-		download(int(number))
+		if "Remake" in regex_match.group(5):
+			cprint(search_result.entries[i].title, 'yellow')
+		else:
+			print(search_result.entries[i].title)
+	cprint("\tSize " + regex_match.group(4), end='\t')
+	cprint("SE " + regex_match.group(1), 'green', end='\t')
+	cprint("LE " + regex_match.group(2), 'red', end='\t')
+	cprint("DLs " + regex_match.group(3))
+
+numbers = input("Choose files to download\n>> ").split()
+
+if not numbers:
+	exit(0)
+else:
+	NULLF = open(devnull, 'w')
+	# Starting 'transmission-daemon', if not already running!
+	if call(["pidof", "transmission-daemon"], stdout=NULLF) == 1:
+		cprint("Starting 'transmission-daemon'... ", end='')
+		td_running = call("transmission-daemon", stdout=NULLF)
+		while(td_running == 1):
+			cprint("failed\nRetrying... ", end='')
+			td_running = call("transmission-daemon", stdout=NULLF)
+		cprint("done")
+	# Download
+	for digit in numbers:
+		s_digit = digit.split('-')
+		if len(s_digit) == 2:
+			for n in range(int(s_digit[0]), int(s_digit[1]) + 1):
+				cprint("Adding file " + str(n) + "...", end='')
+				call(["transmission-remote-cli", search_result.entries[int(n)].link], stdout=NULLF)
+				cprint("done")
+		else:
+			cprint("Adding file " + str(digit) + "...", end='')
+			call(["transmission-remote-cli", search_result.entries[int(digit)].link], stdout=NULLF)
+			cprint("done")
